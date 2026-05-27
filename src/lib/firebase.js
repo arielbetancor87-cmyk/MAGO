e21fb440765cd30a96ab2989a73d9c287a103b9a
arielbetancor87-cmyk <ariel.betancor87@gmail.com>
2026-05-27 19:12:05 -0300

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,6 +1,5 @@
-import { initializeApp }    from 'firebase/app'
-import { getFirestore }     from 'firebase/firestore'
-import { getStorage }       from 'firebase/storage'
+import { initializeApp } from 'firebase/app'
+import { getFirestore }  from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
@@ -11,6 +10,5 @@ const firebaseConfig = {
   appId:             import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
-const app     = initializeApp(firebaseConfig)
-export const db      = getFirestore(app)
-export const storage = getStorage(app)
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
